3461e6d7b5ddcf75aaee897a3a49f8e0a3697f9f
Nazar Horshevikov <rangar.danarim@gmail.com>
2023-02-26 16:40:01 +0200

    UPDATE add mobile version for stage1

--- src/common/components/Stage1/Stage1.tsx
+++ src/common/components/Stage1/Stage1.tsx
@@ -1,8 +1,9 @@
-import React, {useCallback, useEffect, useMemo} from "react"
-import {Controller, useForm} from "react-hook-form"
-import {Data, MyComboBox} from "../form/MyComboBox/MyComboBox"
-import {countryCodes} from "./countryCodes"
-import styles from "./Stage1.module.scss"
+import React, {useCallback, useEffect, useMemo} from "react";
+import {Controller, useForm} from "react-hook-form";
+import {useMobileMediaQuery} from "../../utils/ResponsiveWrappers";
+import {Data, MyComboBox} from "../form/MyComboBox/MyComboBox";
+import {countryCodes} from "./countryCodes";
+import styles from "./Stage1.module.scss";
 
 type Stage1Fields = {
     countryCode: string,
@@ -16,7 +17,9 @@ type Stage1Props = {
 }
 
 const Stage1 = ({countryCode, phoneNumber, handleComplete}: Stage1Props) => {
-    const {register, control, handleSubmit, setValue, formState: {errors}} = useForm<Stage1Fields>()
+    const isMobile = useMobileMediaQuery();
+
+    const {register, control, handleSubmit, setValue, formState: {errors}} = useForm<Stage1Fields>();
 
     const transformedCountryCodes = useMemo(() => (
         countryCodes.map((code) => ({
@@ -24,41 +27,42 @@ const Stage1 = ({countryCode, phoneNumber, handleComplete}: Stage1Props) => {
             name: code.country,
             value: `+${code.code}`,
         } as Data))
-    ), [])
+    ), []);
 
-    const defaultCountryCode = countryCode || transformedCountryCodes[0].value
+    const defaultCountryCode = countryCode || transformedCountryCodes[0].value;
 
     const transformPhoneNumber = useCallback((phoneNumber: string) => {
         const phone = phoneNumber.replaceAll(" ", "")
-                                 .replace(/\D/g, "")
+                                 .replace(/\D/g, "");
 
-        const firstPart = phone.slice(0, 2)
-        const secondPart = phone.slice(2, 5)
-        const thirdPart = phone.slice(5, 7)
-        const fourthPart = phone.slice(7, 9)
-        const fifthPart = phone.slice(9)
+        const firstPart = phone.slice(0, 2);
+        const secondPart = phone.slice(2, 5);
+        const thirdPart = phone.slice(5, 7);
+        const fourthPart = phone.slice(7, 9);
+        const fifthPart = phone.slice(9);
 
         const result = `${firstPart}`
             + `${secondPart ? ` ${secondPart}` : ""}`
             + `${thirdPart ? ` ${thirdPart}` : ""}`
             + `${fourthPart ? ` ${fourthPart}` : ""}`
-            + `${fifthPart ? ` ${fifthPart}` : ""}`
+            + `${fifthPart ? ` ${fifthPart}` : ""}`;
 
-        setValue("phoneNumber", result)
-    }, [setValue])
+        setValue("phoneNumber", result);
+    }, [setValue]);
 
     useEffect(() => {
-        transformPhoneNumber(phoneNumber || "")
-    }, [phoneNumber, setValue, transformPhoneNumber])
+        transformPhoneNumber(phoneNumber || "");
+    }, [phoneNumber, setValue, transformPhoneNumber]);
 
     const handleStage1 = (data: Stage1Fields) => {
-        const phone = data.phoneNumber.replaceAll(" ", "")
+        const phone = data.phoneNumber.replaceAll(" ", "");
 
-        handleComplete(data.countryCode, phone)
-    }
+        handleComplete(data.countryCode, phone);
+    };
 
     return (
-        <form className={styles.stage1} onSubmit={handleSubmit(handleStage1)}>
+        <form className={`${styles.stage1} ${isMobile ? styles.stage1Mobile : ""}`}
+              onSubmit={handleSubmit(handleStage1)}>
             <fieldset className={styles.phoneSet}>
                 <p className={styles.formDesc}>Enter your phone number</p>
                 <div className={styles.phoneWrapper}>
@@ -80,7 +84,7 @@ const Stage1 = ({countryCode, phoneNumber, handleComplete}: Stage1Props) => {
             </fieldset>
             <button type="submit" className={styles.submitButton}>Send code</button>
         </form>
-    )
-}
+    );
+};
 
-export default Stage1
+export default Stage1;
